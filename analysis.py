d85cce1de88886ed6cec196cabc15a858942e316
jocoder22 <okigbookey@gmail.com>
2020-02-11 23:02:57 -0500

    Update

--- analysis.py
+++ analysis.py
@@ -93,3 +93,26 @@ pd.to_pickle(Listings, os.path.join(mydir, "Listings.pkl"))
 # import pickle file
 pandas_pickle = pd.read_pickle(os.path.join(mydir, "Dlistings.pkl"))
 print(pandas_pickle.head())
+
+
+newlist = ['id', 'name', 'city', 'latitude', 'longitude',
+       'is_location_exact', 'property_type', 'room_type', 'accommodates',
+       'bathrooms', 'bedrooms', 'beds', 'bed_type', 'amenities',
+       'price', 'security_deposit', "state",
+       'cleaning_fee', 'guests_included', 'extra_people', 'minimum_nights',
+       'maximum_nights', 'minimum_minimum_nights', 'maximum_minimum_nights',
+       'minimum_maximum_nights', 'maximum_maximum_nights',
+       'minimum_nights_avg_ntm', 'maximum_nights_avg_ntm', 'calendar_updated',
+       'has_availability', 'availability_30', 'availability_60',
+       'availability_90', 'availability_365', 'calendar_last_scraped',
+       'number_of_reviews', 'number_of_reviews_ltm', 'first_review',
+       'last_review', 'review_scores_rating', 'review_scores_accuracy',
+       'review_scores_cleanliness', 'review_scores_checkin',
+       'review_scores_communication', 'review_scores_location',
+       'review_scores_value', 'requires_license',
+        'instant_bookable', 'is_business_travel_ready',
+       'cancellation_policy', 'require_guest_profile_picture',
+       'require_guest_phone_verification', 'calculated_host_listings_count',
+       'calculated_host_listings_count_entire_homes',
+       'calculated_host_listings_count_private_rooms',
+       'calculated_host_listings_count_shared_rooms', 'reviews_per_month']
