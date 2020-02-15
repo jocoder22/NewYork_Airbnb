ac1b556a1e0783d495ad7ae7ac705c7de9f1cc44
jocoder22 <okigbookey@gmail.com>
2020-02-14 21:58:54 -0500

    Add analysi plan

---
+++ analysis_plan.py
@@ -0,0 +1,25 @@
+'''
+################# Analysis PLan #######################
+            # NewYork_Airbnb
+        Writing a Data Scientist Blog Post
+    In patial fulfillment of award of degree
+            Data Science Nanodegree
+                  Udacity
+                  
+1. Formulate research/analysis question
+2. Seek for relevant datasets
+3. Download relevant datasets
+4. Clean datasets
+5. Features extraction and engineering
+6. Exploratory data analysis
+7. Data visualization
+8. Supervised learning
+9. Summary report
+10. Conclusions
+
+
+
+
+
+
+'''
