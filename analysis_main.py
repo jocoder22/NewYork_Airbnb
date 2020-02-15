03e47dcca6bda4588bce0ba545895d7bb7d0be20
jocoder22 <okigbookey@gmail.com>
2020-02-15 07:38:35 -0500

    Update analysis plan

---
+++ analysis_main.py
@@ -0,0 +1,1 @@
+# read in the data and necessary libraries.
--- analysis_plan.py
+++ analysis_plan.py
@@ -5,17 +5,22 @@
     In patial fulfillment of award of degree
             Data Science Nanodegree
                   Udacity
-                  
-1. Formulate research/analysis question(s)
-2. Seek for relevant datasets
-3. Download relevant datasets
-4. Clean datasets
-5. Features extraction and engineering
-6. Exploratory data analysis
-7. Data visualization
-8. Supervised learning
-9. Summary report
-10. Conclusion(s)
+Cross-Industry Standard Process of Data Mining - (CRISP-DM)
+A. Bussiness Understanding                 
+    1. Formulate research/analysis question(s)
+B. Data Understanding
+    1. Seek for relevant datasets
+    2. Download relevant datasets
+C. Data preparation
+    1. Clean datasets
+    2. Features extraction and engineering
+    3. Exploratory data analysis
+    4. Data visualization
+D. Modeling
+    1. Supervised learning
+E. Deployment
+    1. Summary report
+    2. Conclusion(s)
 
 
 
